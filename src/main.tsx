c40cd5ace859c58f4f192475d84021df64600bce
Aruan Galves Amaral <aruangalves@gmail.com>
2024-07-23 16:31:07 -0300

    Setting first routes with BrowserRouter, Routes and Route

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { About } from './templates/About/index.tsx'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>    
   </React.StrictMode>,
 )
---
+++ src/templates/About/index.tsx
@@ -0,0 +1,7 @@
+export const About = () => {
+  return (<div>
+    <h1>About this page</h1>
+    <hr />
+    <p>This is an example for setting a new route and how the implementation changes from React DOM V5.</p>
+  </div>);
+}
