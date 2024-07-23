1102b76ad6c57e7f665bffbb349260d7e466fa6e
Aruan Galves Amaral <aruangalves@gmail.com>
2024-07-23 17:49:44 -0300

    Passing state through <Link /> and useNavigate

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,7 +5,7 @@ export const Menu = () => {
   return (
     <nav id="main-menu">
       <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to="/about" state="You reached about page from menu">About</Link>
       <Link to="/nested">Nested</Link>
       <Link to="/posts">Posts</Link>
       <Link to="/posts/3">Post #3</Link>
--- src/templates/About/index.tsx
+++ src/templates/About/index.tsx
@@ -1,7 +1,13 @@
+import { useLocation } from "react-router-dom";
+
 export const About = () => {
+  const location = useLocation();
+  const {state} = location;
   return (<div>
     <h1>About this page</h1>
     <hr />
     <p>This is an example for setting a new route and how the implementation changes from React DOM V5.</p>
+    {state ? <p>{state}</p> : <br />}
+    
   </div>);
 }
